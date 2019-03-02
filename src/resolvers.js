4362b1c31bab3a8e3b058fdb3db99002794d10a2
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2019-03-02 12:05:50 +0700

    feat(test): add message console

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,155 +1,148 @@
-import {
-	getName,
-} from './helpers';
+import { getName } from "./helpers";
 
-export function resolveGetRange(model, parentFieldName = '') {
+export function resolveGetRange(model, parentFieldName = "") {
+  const modelIdentity = model.identity;
 
-	const modelIdentity = model.identity;
+  return (
+    rootValue,
+    { where = "{}", limit = 100, skip = 0, sort = "" },
+    { request, reqData = {} }
+  ) =>
+    new Promise((resolve, reject) => {
+      console.log("sails-graphql-2 rock!");
+      let whereWithParentId;
 
-	return (
-		rootValue,
-		{ where = '{}', limit = 100, skip = 0, sort = '' },
-		{ request, reqData = {} }
-	) => new Promise((resolve, reject) => {
+      if (rootValue && rootValue.id) {
+        whereWithParentId = Object.assign(JSON.parse(where), {
+          [parentFieldName]: rootValue.id
+        });
 
-		let whereWithParentId;
+        whereWithParentId = JSON.stringify(whereWithParentId);
+      }
 
-		if (rootValue && rootValue.id) {
+      request(
+        {
+          method: "GET",
+          url: `/${modelIdentity}/find`,
+          ...reqData
+        },
+        {
+          where: whereWithParentId || where,
+          limit,
+          skip,
+          sort
+        },
+        (err1, res) => {
+          if (err1) {
+            return reject(err1);
+          }
 
-			whereWithParentId = Object.assign(JSON.parse(where), {
-				[parentFieldName]: rootValue.id
-			});
+          model.count(JSON.parse(where), (err2, count) => {
+            if (err2) {
+              return reject(err2);
+            }
 
-			whereWithParentId = JSON.stringify(whereWithParentId);
-
-		}
-
-		request({
-			method: 'GET',
-			url: `/${modelIdentity}/find`,
-			...reqData
-		}, {
-			where: whereWithParentId || where,
-			limit,
-			skip,
-			sort
-		}, (err1, res) => {
-			if (err1) {
-				return reject(err1);
-			}
-
-			model.count(JSON.parse(where), (err2, count) => {
-				if (err2) {
-					return reject(err2);
-				}
-
-				resolve({
-					page: skip / limit + 1,
-					pages: Math.ceil(count / limit),
-					perPage: limit,
-					total: count,
-					edges: res.body
-				});
-			});
-		});
-
-	});
-
+            resolve({
+              page: skip / limit + 1,
+              pages: Math.ceil(count / limit),
+              perPage: limit,
+              total: count,
+              edges: res.body
+            });
+          });
+        }
+      );
+    });
 }
 
 export function resolveGetSingle(model) {
+  const modelIdentity = model.identity;
 
-	const modelIdentity = model.identity;
-
-	return (
-		rootValue,
-		args,
-		{ request, reqData = {} }
-	) => new Promise((resolve, reject) => {
-		request({
-			method: 'GET',
-			url: `/${modelIdentity}/find`,
-			...reqData
-		}, args, (err, res) => {
-			if (err) {
-				return reject(err);
-			}
-			if (res.body instanceof Array) {
-				res.body = res[0];
-			}
-			resolve(res.body);
-		});
-	});
-
+  return (rootValue, args, { request, reqData = {} }) =>
+    new Promise((resolve, reject) => {
+      request(
+        {
+          method: "GET",
+          url: `/${modelIdentity}/find`,
+          ...reqData
+        },
+        args,
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          if (res.body instanceof Array) {
+            res.body = res[0];
+          }
+          resolve(res.body);
+        }
+      );
+    });
 }
 
 export function resolveCreate(model) {
+  const modelIdentity = model.identity;
+  const { queryName } = getName(model);
 
-	const modelIdentity = model.identity;
-	const { queryName } = getName(model);
-
-	return (
-		rootValue,
-		args,
-		{ request, reqData = {} }
-	) => new Promise((resolve, reject) => {
-		request({
-			method: 'POST',
-			url: `/${modelIdentity}/create`,
-			...reqData
-		}, args[queryName], (err, res) => {
-			if (err) {
-				return reject(err);
-			}
-			resolve(res.body);
-		});
-	});
-
+  return (rootValue, args, { request, reqData = {} }) =>
+    new Promise((resolve, reject) => {
+      request(
+        {
+          method: "POST",
+          url: `/${modelIdentity}/create`,
+          ...reqData
+        },
+        args[queryName],
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(res.body);
+        }
+      );
+    });
 }
 
 export function resolveDelete(model) {
+  const modelIdentity = model.identity;
 
-	const modelIdentity = model.identity;
-
-	return (
-		rootValue,
-		{ id },
-		{ request, reqData = {} }
-	) => new Promise((resolve, reject) => {
-		request({
-			method: 'DELETE',
-			url: `/${modelIdentity}/${id}`,
-			...reqData
-		}, (err, res) => {
-			if (err) {
-				return reject(err);
-			}
-			resolve(res.body);
-		});
-	});
-
+  return (rootValue, { id }, { request, reqData = {} }) =>
+    new Promise((resolve, reject) => {
+      request(
+        {
+          method: "DELETE",
+          url: `/${modelIdentity}/${id}`,
+          ...reqData
+        },
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(res.body);
+        }
+      );
+    });
 }
 
 export function resolveUpdate(model) {
+  const modelIdentity = model.identity;
+  const { queryName } = getName(model);
 
-	const modelIdentity = model.identity;
-	const { queryName } = getName(model);
-
-	return (
-		rootValue,
-		args,
-		{ request, reqData = {} }
-	) => new Promise((resolve, reject) => {
-		request({
-			method: 'PUT',
-			url: `/${modelIdentity}/${args.id}`,
-			...reqData
-		}, args[queryName] , (err, res) => {
-			if (err) {
-				return reject(err);
-			}
-			resolve(res.body);
-		});
-	});
-
+  return (rootValue, args, { request, reqData = {} }) =>
+    new Promise((resolve, reject) => {
+      request(
+        {
+          method: "PUT",
+          url: `/${modelIdentity}/${args.id}`,
+          ...reqData
+        },
+        args[queryName],
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(res.body);
+        }
+      );
+    });
 }
